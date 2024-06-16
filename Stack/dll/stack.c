26032b81d67f0c5c922ef151a64aa1ce6dec2d73
Fikri Aqsha <m.fikriaqsha@gmail.com>
2024-06-16 14:38:53 +0700

    Add push, pop, and peek feature

--- Stack/dll/stack.c
+++ Stack/dll/stack.c
@@ -27,12 +27,31 @@ Node* create_new_node(int data){
 }
 
 void push(Node** head, Node** tail, int data){
+	Node* new_node = create_new_node(data);
+	if(*tail == NULL){
+		*head = *tail = new_node;
+		return;
+	}
+	new_node->prev = *tail;
+	(*tail)->next = new_node;
+	*tail = new_node;
 }
 
 int pop(Node** tail){
+	if(*tail != NULL){
+		Node* node_to_pop = *tail;
+		int pop_value = node_to_pop -> data;
+		*tail = node_to_pop -> prev;
+		(*tail)->next = NULL;
+		free(node_to_pop);
+		return pop_value;
+	}
 }
 
 int peek(Node* tail){
+	if(tail != NULL){
+		return tail->data;
+	}
 }
 
 void free_up_memory(Node** head){
@@ -53,6 +72,10 @@ int main(){
 	push(&head, &tail, 15);
 	push(&head, &tail, 4);
 	push(&head, &tail, 20);
-	
+	print_stack(head);
+	printf("Peek value: %d\n", peek(tail));
+	printf("Pop value: %d\n", pop(&tail));
+	printf("Peek value after pop: %d\n", peek(tail));
+	print_stack(head);
 	free_up_memory(&head);
 }
