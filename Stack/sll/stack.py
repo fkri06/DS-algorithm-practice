fdbc460e11c05c5625e7b4dc63d31330f9f5d1e6
Fikri Aqsha <m.fikriaqsha@gmail.com>
2024-06-16 08:51:05 +0700

    Implement stack using singly linked list in Python

---
+++ Stack/sll/stack.py
@@ -0,0 +1,24 @@
+# Stack implementation using singly linked list.
+# Basic stacks operation:
+#   - Push  : add data at the front of a list.
+#   - Pop   : remove and return the value at the end of a list.
+#   - Top/peek : return the front of a stack's value withuot removing the value.
+
+class Node:
+    def __init__(self. data = None):
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.front = None
+
+    def push(self, value: int):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
