69f61cdc2b475a05ec68c8cbca4cd619fb9fda3e
Fikri Aqsha <m.fikriaqsha@gmail.com>
2024-06-19 13:09:44 +0700

    Implement Heap using Python

---
+++ Trees/Heap/heap.py
@@ -0,0 +1,35 @@
+# Heap implementation using doubly linked list.
+# Basic Heap operation:
+#	- Insert -> insert an element to a heap. Insertion
+#         in a Heap always start from the bottom/end of list/array.
+#
+#	- Remove -> remove the root element from a heap. Deletion
+#         in a Heap always delete the root node, and then from there
+#         fix the heap arrangements.
+#
+
+class Heap:
+    def __init__():
+        self.data = []
+        self.length = 0
+    
+    def find_parent(self, index: int):
+        pass
+
+    def find_left_child(self, index: int):
+        pass
+
+    def find_right_child(self, index: int):
+        pass
+
+    def heap_up(self, index):
+        pass
+
+    def heap_down(self, index):
+        pass
+
+    def insert(self):
+        pass
+
+    def remove(self):
+        pass
