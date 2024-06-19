fc8ce9bf2ba01b44883a1f22adbe385823b2eb0d
Fikri Aqsha <m.fikriaqsha@gmail.com>
2024-06-19 07:47:36 +0700

    Implement Binary Search Tree in Python

---
+++ Trees/BinarySearchTree/binary_search_tree.py
@@ -0,0 +1,42 @@
+# Binary Search Tree implementation using doubly linked list.
+# Basic BST operation:
+#
+#	- Insert -> insert an element to a tree.
+#	- Delete -> remove an element from a tree.
+#	- Search -> searching an element in a tree.
+#	- Traversal:
+#		* Preorder
+#		* Inorder
+#		* Postorder
+#		* Level Order
+
+class BinaryTree:
+    def __init__(self, data = None):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value_to_insert: int):
+        pass
+
+    def search(self, valut_to_search: int):
+        pass
+
+    def remove(self, value_to_delete: int):
+        pass
+     
+    def preorder(self):
+        pass
+
+    def inorder(self):
+        pass
+
+    def postorder(self):
+        pass
+
+    def levelorder(self):
+        pass
