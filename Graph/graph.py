ce199ed206cb014ed9bbfd527910e519511b8253
Fikri Aqsha <m.fikriaqsha@gmail.com>
2024-06-20 12:14:47 +0700

    Implement undirected and unweighted graph in Python using adjacency list technique

---
+++ Graph/graph.py
@@ -0,0 +1,17 @@
+# Undirected and Unweighted Graph implementation using adjacency list.
+
+class Graph:
+    def __init__(self, value = None):
+        self.value = value
+        self.adjacency_vertices = []
+
+    def add_adjecency_vertex(sefl, vertex):
+        pass
+
+    def dfs_traversal():
+        pass
+
+    def bfs_traversal():
+        pass
+
+
